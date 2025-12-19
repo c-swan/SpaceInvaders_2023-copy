0da3a83286405279687e60ce0628797a4ea850ed
c-swan <77839064+c-swan@users.noreply.github.com>
2025-12-19 15:07:20 +0100

    changed loading entity files, working on GameState FSM

---
+++ SpaceInvaders_2023/Source/GameState.cpp
@@ -0,0 +1,8 @@
+//
+//  GameState.cpp
+//  SpaceInvaders_2023
+//
+//  Created by Carl Swanberg on 2025-12-19.
+//
+
+#include "GameState.hpp"
---
+++ SpaceInvaders_2023/Source/GameState.hpp
@@ -0,0 +1,13 @@
+//
+//  GameState.hpp
+//  SpaceInvaders_2023
+//
+//  Created by Carl Swanberg on 2025-12-19.
+//
+
+#ifndef GameState_hpp
+#define GameState_hpp
+
+#include <stdio.h>
+
+#endif /* GameState_hpp */
