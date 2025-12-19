a4859e1514b629214ba2aeb8361bed7e7227dd59
c-swan <77839064+c-swan@users.noreply.github.com>
2025-12-19 19:15:24 +0100

    gamestate, moved Entities

---
+++ SpaceInvaders_2023/Source/Entities.hpp
@@ -0,0 +1,13 @@
+//
+//  Entities.hpp
+//  SpaceInvaders_2023
+//
+//  Created by Carl Swanberg on 2025-12-19.
+//
+
+#ifndef Entities_hpp
+#define Entities_hpp
+
+#include <stdio.h>
+
+#endif /* Entities_hpp */
