eda4138dc1d0dc5ccce87dd804d6cef4d9be1821
c-swan <77839064+c-swan@users.noreply.github.com>
2025-12-19 12:27:03 +0100

    Initial Commit

---
+++ SpaceInvaders_2023/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SpaceInvaders_2023
+//
+//  Created by Carl Swanberg on 2025-12-19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return EXIT_SUCCESS;
+}
