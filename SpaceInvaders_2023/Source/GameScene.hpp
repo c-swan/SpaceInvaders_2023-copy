9796d890f0f5ec8ef3a00e521f04d2b0476e33f3
c-swan <77839064+c-swan@users.noreply.github.com>
2025-12-20 22:30:50 +0100

    separated GameStates into different functions, moved stuff to constructors etc, edited for ranged loops

---
+++ SpaceInvaders_2023/Source/GameScene.hpp
@@ -0,0 +1,13 @@
+//
+//  GameScene.hpp
+//  SpaceInvaders_2023
+//
+//  Created by Carl Swanberg on 2025-12-20.
+//
+
+#ifndef GameScene_hpp
+#define GameScene_hpp
+
+#include <stdio.h>
+
+#endif /* GameScene_hpp */
