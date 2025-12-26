3870c28472ba148fda6ab27b61c38ff6486d89c0
c-swan <77839064+c-swan@users.noreply.github.com>
2025-12-27 00:04:01 +0100

    applied Agorithms, FAFO

---
+++ SpaceInvaders_2023/Source/ErrorHandling.h
@@ -0,0 +1,12 @@
+//
+//  ErrorHandling.h
+//  SpaceInvaders_2023
+//
+//  Created by Carl Swanberg on 2025-12-26.
+//
+
+#ifndef ErrorHandling_h
+#define ErrorHandling_h
+
+
+#endif /* ErrorHandling_h */
