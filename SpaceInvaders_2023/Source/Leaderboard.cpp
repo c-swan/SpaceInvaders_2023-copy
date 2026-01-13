cdc732e58036c8f2fb746a54e0563fa8e5c9396a
c-swan <77839064+c-swan@users.noreply.github.com>
2026-01-13 23:27:37 +0100

    small changes, adding infrastructure for AlienSwarm

---
+++ SpaceInvaders_2023/Source/Leaderboard.cpp
@@ -0,0 +1,8 @@
+//
+//  Leaderboard.cpp
+//  SpaceInvaders_2023
+//
+//  Created by Carl Swanberg on 2026-01-13.
+//
+
+#include "Leaderboard.hpp"
---
+++ SpaceInvaders_2023/Source/Leaderboard.hpp
@@ -0,0 +1,13 @@
+//
+//  Leaderboard.hpp
+//  SpaceInvaders_2023
+//
+//  Created by Carl Swanberg on 2026-01-13.
+//
+
+#ifndef Leaderboard_hpp
+#define Leaderboard_hpp
+
+#include <stdio.h>
+
+#endif /* Leaderboard_hpp */
